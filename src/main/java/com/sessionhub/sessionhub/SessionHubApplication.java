d469ccd69a958a56bb191355d1ebb030c7663315
dragon0622 <iamdragon.dev@gmail.com>
2025-03-05 20:15:12 +0900

    setting

---
+++ src/main/java/com/sessionhub/sessionhub/SessionHubApplication.java
@@ -0,0 +1,13 @@
+package com.sessionhub.sessionhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionHubApplication.class, args);
+    }
+
+}
